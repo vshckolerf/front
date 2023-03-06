aaafa4da4b572930aaf075aab3852c01b93409e8
MIMBOL228 <aekemerov@yandex.ru>
2023-03-07 00:37:17 +0500

    Add authorization by JWT

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@
  * App.tsx
  * Facebook, продукт компании Meta, которая признана экстремистской организацией в России
  */
+
 import { Route, Routes } from "react-router-dom";
 import { IndexPage } from "./Pages/IndexPage";
 import { SchedulePage } from "./Pages/SchedulePage";
--- src/Pages/IndexPage.tsx
+++ src/Pages/IndexPage.tsx
@@ -1,10 +1,52 @@
+import { ThemeSwitcherComponent } from "../Components/ThemeSwitcherComponent";
+import { API_URL } from "../config";
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 import logoImg from "../assets/logo.svg";
 import "../css/index.css";
-import { ThemeSwitcherComponent } from "../Components/ThemeSwitcherComponent";
+import "react-toastify/dist/ReactToastify.css";
 
 export function IndexPage() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const auth = () => {
+        fetch(API_URL + "auth/login", {
+            method: "post",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password,
+            }),
+        }).then((resp: Response) => {
+            if (resp.ok) {
+                resp.json().then((respObj) => {
+                    localStorage.setItem("jwt", respObj.token);
+                    toast.success("Успешно!", {
+                        position: toast.POSITION.BOTTOM_LEFT,
+                        autoClose: 1000,
+                        onClose: () => {
+                            navigate("/schedule");
+                        },
+                    });
+                });
+            } else {
+                toast.error("Неверные почта или пароль!", {
+                    position: toast.POSITION.BOTTOM_LEFT,
+                    autoClose: 2000,
+                });
+            }
+        });
+    };
+
     return (
         <div className="wrapper">
+            <ToastContainer limit={3} />
             <div id="auth">
                 <img
                     src={logoImg}
@@ -17,6 +59,8 @@ export function IndexPage() {
                         className="auth_inp"
                         required
                         placeholder="Почта"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
                     />
                     <input
                         type="password"
@@ -24,8 +68,14 @@ export function IndexPage() {
                         className="auth_inp"
                         required
                         placeholder="Пароль"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
                     />
-                    <button id="button">Войти</button>
+                    <button
+                        id="button"
+                        onClick={auth}>
+                        Войти
+                    </button>
                 </div>
                 <p id="rem_pass">
                     Для смены пароля свяжитесь с{" "}
