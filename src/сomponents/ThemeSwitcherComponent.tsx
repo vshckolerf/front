b6131ef50abe8d99ed7ede3153eb379af808ec5f
MIMBOL228 <aekemerov@yandex.ru>
2023-03-15 00:13:56 +0500

    React adaptation

--- src/сomponents/ThemeSwitcherComponent.tsx
+++ src/сomponents/ThemeSwitcherComponent.tsx
@@ -18,15 +18,15 @@ export function ThemeSwitcherComponent() {
                         cy="16"
                         r="6"
                         stroke="#535358"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
                     />
                     <path
                         stroke="#535358"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
                         d="M16 6V3M8.929 8.929L6.808 6.808M6 16H3M16 29v-3M25.192 25.192l-2.121-2.121M29 16h-3M8.929 23.071l-2.121 2.121M25.192 6.808l-2.12 2.121"
                     />
                 </svg>
