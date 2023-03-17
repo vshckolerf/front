2613bba43fde6ac806fb370c46609b6fcb2c3b35
MIMBOL228 <aekemerov@yandex.ru>
2023-03-17 07:20:24 +0500

    Horizontal scroll in /schedule

--- src/pages/SchedulePage.tsx
+++ src/pages/SchedulePage.tsx
@@ -6,7 +6,26 @@ import plusImg from "../assets/plus.svg";
 import "../css/schedule.css";
 import { NavbarComponent } from "../сomponents/NavbarComponent";
 import { DayComponent } from "../сomponents/DayComponent";
-
+import {useEffect, useRef} from "react";
+export function useHorizontalScroll() {
+    const elRef = useRef<null | HTMLDivElement>(null);
+    useEffect(() => {
+        const el = elRef.current;
+        if (el) {
+            const onWheel = (e: { deltaY: number; preventDefault: () => void; }) => {
+                if (e.deltaY == 0) return;
+                e.preventDefault();
+                el.scrollTo({
+                    left: el.scrollLeft + e.deltaY*3,
+                    behavior: "smooth"
+                });
+            };
+            el.addEventListener("wheel", onWheel);
+            return () => el.removeEventListener("wheel", onWheel);
+        }
+    }, []);
+    return elRef;
+}
 export function SchedulePage() {
     const navigate = useNavigate();
     const exit = () => {
@@ -18,13 +37,14 @@ export function SchedulePage() {
     );
     if (isExpired) exit();
     const userInfo = decodedToken;
+    const scrollRef = useHorizontalScroll();
     return (
         <>
             <NavbarComponent
                 userInfo={userInfo}
                 onExit={exit}
             />
-            <div className="days_wrapper">
+            <div className="days_wrapper" ref={scrollRef} style={{overflow: "auto" }}>
                 <DayComponent dow={0} />
                 <DayComponent dow={1} />
                 <DayComponent dow={2} />
