b657fcf0b3e49f3a834e28ff6bb7999f70162f5a
MIMBOL228 <aekemerov@yandex.ru>
2023-03-17 17:57:59 +0500

    Create fetches

---
+++ src/fetches/dayFetch.ts
@@ -0,0 +1,26 @@
+interface ILesson {
+    start: string;
+    end: string;
+}
+
+export function dayFetch(order: number, token: string) {
+    return new Promise<ILesson[]>((resolve, reject) => {
+        fetch(import.meta.env.VITE_API_URL + `schedule/${order}`, {
+            method: "get",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            }
+        }).then((resp: Response) => {
+            if (resp.ok) {
+                resp.json().then((respObj) => {
+                    resolve(respObj);
+                });
+            } else {
+                reject(resp);
+            }
+        });
+    })
+
+}
