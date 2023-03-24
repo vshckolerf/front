0a16c0a9c59195ca36ce4f9155482d9a4aff7b58
Александр <59563952+MIMBOL228@users.noreply.github.com>
2023-03-24 09:43:49 +0500

    Dev (#26)
    * Lint
    
    * Add settings for stage branch
    
    * Large refactor
    
    * Lint
    
    * Move Lint action to npm
    
    * FUCK, WebStorm !!!
    
    Signed-off-by: MIMBOL228 <aekemerov@yandex.ru>
    
    ---------
    
    Signed-off-by: MIMBOL228 <aekemerov@yandex.ru>

--- src/сomponents/DayComponent.tsx
+++ src/сomponents/DayComponent.tsx
@@ -1,144 +1,114 @@
 import plusImg from "../assets/plus.svg";
 import React, {useLayoutEffect, useState} from "react";
-import removeImg from "../assets/remove.svg";
 import {dayFetch} from "../fetches/dayFetch";
 import {setDayFetch} from "../fetches/setDayFetch";
 import "../css/DayComponent.css";
-interface IDayParams {
-  dow: number;
-}
-interface ILesson {
-  start: string;
-  end: string;
-}
-const weekDays = [
-  "Понедельник",
-  "Вторник",
-  "Среда",
-  "Четверг",
-  "Пятница",
-  "Суббота",
-  "Воскресенье",
-];
-export function DayComponent({ dow }: IDayParams) {
-  const [lessons, setLessons] = useState<ILesson[] | null>([]);
-  useLayoutEffect(() => {
-    dayFetch(dow)
-        .then((lessons: ILesson[]) => {
-          setLessons(lessons);
-        }, (resp) => {
-          console.log("err" + dow, resp);
-        });
-  }, []);
-  const pushToBack = async (less: ILesson[]) => {
-    setDayFetch(dow, less, localStorage.getItem("jwt") as string)
-        .then((resp: string) => {
-          console.log("sus" + dow, resp);
-        }, (resp: string) => {
-          console.log("err" + dow, resp);
-        });
-  }
-  //useEffect(pushToBack, [lessons]);
+import {LessonComponent} from "./LessonComponent";
+import {ILesson} from "../interfaces/ILesson";
 
-  const changeTime = async (order: number, type: boolean, time: string) => {
-    await setLessons((prevState: ILesson[] | null) => {
-      if (prevState == null) return [];
-      const ret = prevState.map((v, k) => {
-        if (k == order) {
-          if (type) {
-            return {...v, end: time}
-          } else {
-            return {...v, start: time}
-          }
-        } else {
-          return v;
+
+interface IDayParams {
+    dow: number;
+}
+
+
+const weekDays = [
+    "Понедельник",
+    "Вторник",
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота",
+    "Воскресенье",
+];
+
+export function DayComponent({dow}: IDayParams) {
+    const [lessons, setLessons] = useState<ILesson[] | null>([]);
+    useLayoutEffect(() => {
+        dayFetch(dow)
+            .then((lessons: ILesson[]) => {
+                setLessons(lessons);
+            }, (resp) => {
+                console.log("err" + dow, resp);
+            });
+    }, []);
+    const pushToBack = async (less: ILesson[]) => {
+        setDayFetch(dow, less, localStorage.getItem("jwt") as string)
+            .then((resp: string) => {
+                console.log("sus" + dow, resp);
+            }, (resp: string) => {
+                console.log("err" + dow, resp);
+            });
+    }
+    //useEffect(pushToBack, [lessons]);
+    const timeManage = {
+        set: async (order: number, type: boolean, time: string) => {
+            await setLessons((prevState: ILesson[] | null) => {
+                if (prevState == null) return [];
+                const ret = prevState.map((v, k) => {
+                    if (k == order) {
+                        if (type) {
+                            return {...v, end: time}
+                        } else {
+                            return {...v, start: time}
+                        }
+                    } else {
+                        return v;
+                    }
+                });
+                pushToBack(ret);
+                return ret;
+            });
+            console.log(lessons);
+        },
+        add: async () => {
+            await setLessons((prevState: ILesson[] | null) => {
+                if (prevState == null) return [{
+                    start: "10:00",
+                    end: "11:00",
+                }];
+                const ret = [...prevState, {
+                    start: "10:00",
+                    end: "11:00",
+                }];
+                pushToBack(ret);
+                return ret;
+            });
+            console.log(lessons);
+        },
+        remove: async (order: number) => {
+            await setLessons((prevState: ILesson[] | null) => {
+                if (prevState == null) return [];
+                const ret = prevState.filter((value, index) => index !== order);
+                pushToBack(ret);
+                return ret;
+            });
+            console.log(lessons);
         }
-      });
-      pushToBack(ret);
-      return ret;
-    });
-    console.log(lessons);
-  };
-  const addLesson = async () => {
-    await setLessons((prevState: ILesson[] | null) => {
-      if (prevState == null) return [{
-        start: "10:00",
-        end: "11:00",
-      }];
-      const ret = [...prevState, {
-        start: "10:00",
-        end: "11:00",
-      }];
-      pushToBack(ret);
-      return ret;
-    });
-    console.log(lessons);
-  };
-  const removeLesson = async (order: number) => {
-    await setLessons((prevState: ILesson[] | null) => {
-      if (prevState == null) return [];
-      const ret = prevState.filter((value, index) => index !== order);
-      pushToBack(ret);
-      return ret;
-    });
-    console.log(lessons);
-  };
-  return (
-      <div
-          className="day"
-      id="first-day">
-      <div className="day_top">
-        <p className="static">Расписание</p>
-        <p className="name">{weekDays[dow]}</p>
-      </div>
-      <div className="lessons_wrapper">
-        {lessons?.map((obj: ILesson,key)=>{
-          return(<div className="lesson" key={key}>
-            <div className="left">
-              <p className="number">{key+1}-й</p>
+    }
+
+    return (
+        <div className="day">
+            <div className="day_top">
+                <p className="static">Расписание</p>
+                <p className="name">{weekDays[dow]}</p>
             </div>
-            <div className="right">
-              <div className="time">
-                <input
-                  type="time"
-                  className="les_time"
-                  value={lessons[key].start}
-                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => changeTime(key,false,event.target.value)}
-                />
-                <p className="wall">:</p>
-                <input
-                  type="time"
-                  className="les_time"
-                  value={lessons[key].end}
-                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => changeTime(key,true,event.target.value)}
-                />
-              </div>
-              <div className="remove">
+            <div className="lessons_wrapper">
+                {lessons?.map((obj: ILesson, key) => {
+                    return (<LessonComponent lesson={obj} key={key} index={key} timeManage={timeManage}/>);
+                })}
+            </div>
+            <div className="add">
                 <button
-                  onClick={()=>{removeLesson(key);}}
+                    onClick={timeManage.add}
                 >
-                  <img
-                    className="remove"
-                    src={removeImg}
-                    alt="img"
-                  />
+                    <img
+                        src={plusImg}
+                        className="add"
+                        alt="img"
+                    />
                 </button>
-              </div>
             </div>
-          </div>)
-        })}
-      </div>
-      <div className="add">
-        <button
-          onClick={addLesson}
-        >
-          <img
-            src={plusImg}
-            className="add"
-            alt="img"
-          />
-        </button>
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
--- src/сomponents/NavbarComponent.tsx
+++ src/сomponents/NavbarComponent.tsx
@@ -1,43 +1,43 @@
 import React from "react"
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import logoImg from "../assets/logo.svg";
-import { IUser } from "../interfaces/IUser";
+import {IUser} from "../interfaces/IUser";
 
 interface INavbarParams {
     userInfo: IUser | null;
 }
 
-export function NavbarComponent({ userInfo}: INavbarParams) {
-  const navigate = useNavigate();
-  const exit = () => {
-    localStorage.removeItem("jwt");
-    navigate("/");
-  };
-  return (
-    <div className="nav_wrapper">
-      <div className="left_nav">
-        <img
-          src={logoImg}
-          alt=""
-          id="nav_logo"
-        />
-        <nav>
-          <ul>
-            <li className="nav_item active">Расписание</li>
-            <li className="nav_item">Объявления</li>
-            <li className="nav_item">Пользователи</li>
-            <li className="nav_item">Файл</li>
-          </ul>
-        </nav>
-      </div>
-      <div className="right_nav">
-        <p className="name">{userInfo?.fullName}</p>
-        <button
-          id="exit"
-          onClick={exit}>
+export function NavbarComponent({userInfo}: INavbarParams) {
+    const navigate = useNavigate();
+    const exit = () => {
+        localStorage.removeItem("jwt");
+        navigate("/");
+    };
+    return (
+        <div className="nav_wrapper">
+            <div className="left_nav">
+                <img
+                    src={logoImg}
+                    alt=""
+                    id="nav_logo"
+                />
+                <nav>
+                    <ul>
+                        <li className="nav_item active">Расписание</li>
+                        <li className="nav_item">Объявления</li>
+                        <li className="nav_item">Пользователи</li>
+                        <li className="nav_item">Файл</li>
+                    </ul>
+                </nav>
+            </div>
+            <div className="right_nav">
+                <p className="name">{userInfo?.fullName}</p>
+                <button
+                    id="exit"
+                    onClick={exit}>
                     Выйти
-        </button>
-      </div>
-    </div>
-  );
+                </button>
+            </div>
+        </div>
+    );
 }
