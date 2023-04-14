6714f6f9d817e32f81df8d26e8ba5fa9302fb744
kir <blindmonk46@gmail.com>
2023-04-14 22:13:37 +0300

    add: announcement page with form

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,20 @@
  */
 
 import React from "react";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./css/color-light.css";
 
 import "./css/main.css";
 import IndexPage from "./pages/IndexPage";
 import SchedulePage from "./pages/SchedulePage";
+import AnnouncementPage from "./pages/AnnouncementPage";
 
 export default function App() {
     return (
         <Routes>
-            <Route
-                path="/"
-                element={<IndexPage/>}
-            />
-            <Route
-                path="/schedule"
-                element={<SchedulePage/>}
-            />
+            <Route path="/" element={<IndexPage />} />
+            <Route path="/schedule" element={<SchedulePage />} />
+            <Route path="/announcement" element={<AnnouncementPage />} />
         </Routes>
     );
 }
